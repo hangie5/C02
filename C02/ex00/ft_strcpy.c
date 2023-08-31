6d63ac1e86c3e7bb6a92249a76ba642b9391584f
hangie5 <142239368+hangie5@users.noreply.github.com>
2023-08-31 19:28:03 +0200

    Update ft_strcpy.c

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -2,14 +2,17 @@
 #include <string.h> //this header provides functions and macros for various string manipulations. mainly used for any functions starting with str
 //for example strcpy, strlen, strcat etc.
 
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src) //function to copy the content of one string (src) to another (dest)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i] != '\0') //checks whether the character at the current position 'i' in the character array 'src' is not equal to the null terminiator '\0'
+		//src[i] accesses the character at the index 'i' in the array 'src'. 
+		//a string is an array of characters amd each character in the array is accessed using an index.
+		//'\0' is the null terminator character. the null terminator is used to mark the end of a string. the character has the ASCII value 0.
 	{
-		dest[i] = src[i];
+		dest[i] = src[i]; //this line is copying the character at index 'i' from the 'src' string to the corresponding index 'i'
 		i++;
 	}
 	dest[i] = '\0';

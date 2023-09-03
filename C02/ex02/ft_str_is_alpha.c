d95295a9edbf6dca72a1ae1efbb15db4174304c6
hangie5 <142239368+hangie5@users.noreply.github.com>
2023-09-03 17:50:13 +0200

    Update ft_str_is_alpha.c

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -11,17 +11,19 @@ int	ft_str_is_alpha(char *str) //define a function called ft_str_is_alpha that t
 	}
 	while (str[i] != '\0') //start a loop that continues until the end of the string (null terminator)
 	{
-		if ((str[i] >= 'A' && str [i] <= 'Z')
-			|| (str [i] >= 'a' && str [i] <= 'z'))
-			i++;
+		if ((str[i] >= 'A' && str [i] <= 'Z') //check if the current character is un uppercase letter ('A' to 'Z') 
+			|| (str [i] >= 'a' && str [i] <= 'z')) // or a lowercase letter ('a' to 'z')
+			i++; //if it's a letter, move to the next character in the string
 		else
-			return (0);
+			return (0); //if the current character is not a letter, return 0, indicating the string contains non-alphabetic characters
 	}	
-	return (1);
+	return (1); //if the loop completes without encountering non-alphabetic characters, return 1
 }
+
 int	main()
 {
-	printf("%d", ft_str_is_alpha("abcdefghijkl"));
-	printf("\n%d", ft_str_is_alpha("abc1defghijkl"));
-	printf("\n%d", ft_str_is_alpha("-_134556efghij67"));
+	//test the ft_str_is_alpha function with different input strings and print the results
+	printf("%d", ft_str_is_alpha("abcdefghijkl")); //should print '1' (contains only alphabetic characters)
+	printf("\n%d", ft_str_is_alpha("abc1defghijkl")); //should print '0' (contains a digit '1')
+	printf("\n%d", ft_str_is_alpha("-_134556efghij67")); //should print '0' (contains non-alphabetic characters)
 }
